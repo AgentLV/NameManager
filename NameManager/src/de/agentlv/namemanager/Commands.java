96838239aaccfd9bd5b6b0f9d537c5b7eebfe9f2
AgentLV <AgentLV@users.noreply.github.com>
2015-07-29 18:57:11 +0200

    Forget a §

--- NameManager/src/de/agentlv/namemanager/Commands.java
+++ NameManager/src/de/agentlv/namemanager/Commands.java
@@ -294,7 +294,7 @@ public class Commands implements CommandExecutor {
 						sender.sendMessage("§3/nm group remove <group>  §7§  §bRemoves a group");
 						sender.sendMessage("§3/nm group list  §7§  §bDisplays all valid groups");
 						sender.sendMessage("§3/nm group rainbow <group>  §7§  §bToggles rainbow name for all group members §c(could cause lag)");
-						sender.sendMessage("33/nm group add <player> <group>  §7§  §bTemporarily add a player to a group");
+						sender.sendMessage("§3/nm group add <player> <group>  §7§  §bTemporarily add a player to a group");
 						sender.sendMessage("");
 					} else {
 						sender.sendMessage(invalidPermission);
